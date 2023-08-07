ba90f2cd77d961d52f9a437a24b87b0dbedac008
fwax <fwax.pro@gmail.com>
2023-08-07 14:59:31 +0000

    test: add temperatures: choose the right temperature

--- src/codingame/Temperatures/index.test.js
+++ src/codingame/Temperatures/index.test.js
@@ -16,6 +16,13 @@ describe('CodinGame: Temperatures', function () {
         assert.strictEqual(result, expect);
     });
 
+    it('choose the right temperature', function () {
+        const arr = [ '42', '-5', '12', '21', '5', '24' ];
+        const expect = '5';
+        const result = numberPositiveCloserToZero(arr);
+        assert.strictEqual(result, expect);
+    });
+
     it('no temperatures', function () {
         const arr = [''];
         const expect = 0;
